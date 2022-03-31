740ace882218bb894dfea59c26cde40d511e7ecf
Dennis Maas <dennis.maas@track.de>
2022-03-31 18:21:18 +0200

    gameplay functioning

--- app.js
+++ app.js
@@ -131,20 +131,40 @@ const showMessage = (message) => {
   setTimeout(()=> messageDisplay.removeChild(messageElement), 2000)
 }
 
+
+const addColorToKey = (keyLetter, color) => {
+  const key = document.getElementById(keyLetter);
+  key.classList.add(color);
+}
+
 const flipTile = () => {
   const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
-  rowTiles.forEach((tile, index) => {
-    const dataLetter = tile.getAttribute('data');
+  let checkWordle = wordle;
+  const guess = [];
 
-    setTimeout(() => {
-      tile.classList.add('flip')
-    if (dataLetter === wordle[index]){
-      tile.classList.add('green-overlay');
-    } else if (wordle.includes(dataLetter)){
-      tile.classList.add('yellow-overlay')
-    } else {
-      tile.classList.add('grey-overlay')
+  rowTiles.forEach(tile => {
+    guess.push({letter: tile.getAttribute('data'), color: 'grey-overlay'})
+  })
+
+  guess.forEach((guess, index) => {
+    if (guess.letter === wordle[index]){
+      guess.color = 'green-overlay';
+      checkWordle = checkWordle.replace(guess.letter, '')
     }
+  })
+  guess.forEach(guess => {
+    if (checkWordle.includes(guess.letter)){
+      guess.color = 'yellow-overlay';
+      checkWordle = checkWordle.replace(guess.letter, '')
+
+    }
+  })
+
+  rowTiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add((guess[index].color))
+      addColorToKey(guess[index].letter, guess[index].color)
     }, 300 * index)
   })
 }
+
